5571271e26c4ea80e6e91c43a8295103ffa7c187
Yash087 <yashbhandare07@gmail.com>
2021-12-23 15:43:16 +0530

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
